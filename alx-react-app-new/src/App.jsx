29cb345cb28cc782769b8b87f468956daa423108
Zainab Umar Ibrahim <xieumar@gmail.com>
2025-07-18 13:06:27 +0100

    initial commit for alx-react-app-new

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+        <p>© 2023 City Lovers</p>
+     </footer>
+    </div>
+  )
+}
+
+export default Footer
