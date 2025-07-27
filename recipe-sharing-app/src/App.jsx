5b371f2f25fca196f998cf734d354368ed9ba81e
Zainab Umar Ibrahim <xieumar@gmail.com>
2025-07-27 17:02:29 +0100

    initial commit for recipe sharing app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <AddRecipeForm />
+      <RecipeList />
+    </>
+  )
+}
+
+export default App
