52164a602963d531d60a06a487924d6e46e2b45e
Zainab Umar Ibrahim <xieumar@gmail.com>
2025-08-09 21:17:22 +0100

    made changes

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img src="https://placehold.co/150" alt="User" className=" rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"/>
       <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
       <p className=" text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
