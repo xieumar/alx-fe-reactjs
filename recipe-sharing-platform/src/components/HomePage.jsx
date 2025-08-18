05e3166bc2fa4da38eece2e1d4529ce363eb3908
Zainab Umar Ibrahim <xieumar@gmail.com>
2025-08-18 18:07:32 +0100

    added recipe detail

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 import recipesData from '../data.json'
 
 function HomePage() {
     const [recipes, setRecipes] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState("");
 
     useEffect(() => {
         setRecipes(recipesData);
     }, [])
   return (
    <div className='flex flex-col items-center justify-center min-h-screen px-4'>
-    <h1 className='text-3xl font-bold mb-4'>Recipe <span className=' text-yellow-300'>Sharing</span> Platform</h1>
+    <h1 className='text-3xl font-bold mb-4'>Recipe <span className=' text-yellow-400'>Sharing</span> Platform</h1>
      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 p-4'>
-      {recipes.map(
+      {recipesData.map(
         (recipe) => (
-            <div key={recipe.id} className="recipe-card p-2 flex flex-col items-center gap-2 bg-white shadow-md hover:shadow-lg hover:scale-105 transition-all rounded-2xl">
+            <Link key={recipe.id} to={`/recipe/${recipe.id}`} className='block'>
+                <div key={recipe.id} className="recipe-card p-2 flex flex-col items-center gap-2 bg-white shadow-md hover:shadow-lg hover:scale-105 transition-all rounded-2xl">
                 <img src={recipe.image} alt={recipe.title} className=' w-[300px] h-[300px] bg-center rounded-2xl'/>
                 <h1 className=' text-xl font-semibold '>{recipe.title}</h1>
                 <p className=' text-base text-center'>{recipe.summary}</p>
             </div>
+            </Link>
         )
       )}
     </div>
---
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import recipesData from "../data.json";
+
+function RecipeDetail() {
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState([]);
+
+  useEffect(() => {
+    const foundRecipe = recipesData.find(
+      (recipe) => recipe.id === parseInt(id)
+    );
+    setRecipe(foundRecipe);
+  }, [id]);
+
+  if (!recipe) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <p className="text-3xl ">Oops! Recipe not found.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 ">
+      <h1 className="text-3xl font-bold mb-4 ">
+        Recipe <span className=" text-yellow-400">Details</span>
+      </h1>
+      <div className="recipe-detail flex flex-col items-center gap-2">
+        <h1 className=" text-xl text-center p-4 font-semibold ">
+          {recipe.title} Details
+        </h1>
+        <img
+          src={recipe.image}
+          alt={recipe.title}
+          className="w-[300px] sm:w-[450px] h-[250px] sm:h-[350px] bg-center rounded-sm"
+        />
+        <p className=" text-base text-justify p-6 w-[340px] sm:w-lg">
+          {recipe.details}
+        </p>
+        <h2 className=" text-lg font-semibold">Ingredients</h2>
+        <ul className="list-disc list-inside">
+          {recipe?.ingredients?.map((ingredient, index) => (
+            <li key={index} className="text-base w-sm ">
+              {ingredient}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default RecipeDetail;
